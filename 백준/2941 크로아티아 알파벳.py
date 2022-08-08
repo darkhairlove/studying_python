dc29d66b10a5fcc6e9b4045aa745aa5f18f3d55d
su <suqkr12@gmail.com>
2022-08-08 19:15:23 +0900

    백준

--- 백준/2941 크로아티아 알파벳.py
+++ 백준/2941 크로아티아 알파벳.py
@@ -1,20 +1,6 @@
-arr = list(input())
-ans = 0
-for i in arr:
-    if i in ("A", "B", "C"):
-        ans += 3
-    elif i in ("D", "E", "F"):
-        ans += 4
-    elif i in ("G", "H", "I"):
-        ans += 5
-    elif i in ("J", "K", "L"):
-        ans += 6
-    elif i in ("M", "N", "O"):
-        ans += 7
-    elif i in ("P", "Q", "R", "S"):
-        ans += 8
-    elif i in ("T", "U", "V"):
-        ans += 9
-    elif i in ("W", "X", "Y", "Z"):
-        ans += 10
-print(ans)
+a = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+b = input()
+
+for i in a:
+    b = b.replace(i, "a")
+print(len(b))
