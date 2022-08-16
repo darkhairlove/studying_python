4b9f8e0be34e995b9fda3d3df70001d574ccfdbb
su <suqkr12@gmail.com>
2022-08-16 23:27:38 +0900

    백준

--- 백준/2839 설탕 배달.py
+++ 백준/2839 설탕 배달.py
@@ -1,7 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-
 num = int(input())
 count = 0
 
@@ -13,5 +12,5 @@ while True:
         break
     num -= 3
     count += 1
-if num < 0 or num == 1 or num == 2:
+if num < 0 or num == 1:
     print("-1")
