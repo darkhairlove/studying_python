1f39b8e04f4f19080bf49c1c429ebc65904167b1
su <suqkr12@gmail.com>
2022-11-21 00:48:21 +0900

    벡준

--- 백준/1564 팩토리얼5.py
+++ 백준/1564 팩토리얼5.py
@@ -1,11 +1,17 @@
 import math
 import sys
+arr = []
 input = sys.stdin.readline
 n = int(input())
 fact = str(math.factorial(n))
-for i in fact[::-5]:
+cnt = 0
+for i in fact[::-1]:
     if i == '0':
         continue
-    else:
-        print(i)
+    if cnt == 5:
         break
+    else:
+        arr.append(i)
+        cnt += 1
+for a in arr:
+    print(a, end='')
