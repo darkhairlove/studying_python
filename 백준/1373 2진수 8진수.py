ec40e229b5793cb19f284a11ecf0f5ecd69b140d
su <suqkr12@gmail.com>
2022-12-24 23:29:17 +0900

    백준_2022_12_24

---
+++ 백준/1373 2진수 8진수.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/1373
+# num = int(input())
+
+# arr2 = list(str(num))
+# a = len(str(num))-1
+# arr = 0
+# for i, k in enumerate(arr2):
+#     arr += int(k) * (2**(a-i))
+
+# arr//8
+
+print(oct(int(input(), 2))[2:])
