23d8087c5cb586a68643bec3085abae86d3f1164
su <suqkr12@gmail.com>
2022-12-26 23:46:27 +0900

    백준_2022_12_26

---
+++ 백준/9093 단어 뒤집기.py
@@ -0,0 +1,9 @@
+num = int(input())
+
+for i in range(num):
+    arr = list(map(str, input().split()))
+    arr2 = list()
+    for i in arr:
+        arr2.append(i[::-1])
+    print(" ".join(arr2))
+    continue
