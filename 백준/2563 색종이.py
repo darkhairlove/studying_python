ac9c02ac60ab94a731e4614c1ca4e959e2335298
su <suqkr12@gmail.com>
2023-01-12 23:59:19 +0900

    백준_2023_01_12

---
+++ 백준/2563 색종이.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+arr = [[0] * 100 for _ in range(100)]
+
+for _ in range(num):
+    y, x = map(int, input().split())
+
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            arr[i][j] = 1
+
+result = 0
+for k in range(100):
+    result += arr[k].count(1)
+print(result)
