94810a3f9c83e112506dce726fe217828c03a698
su <suqkr12@gmail.com>
2023-03-10 00:04:22 +0900

    백준_2023_03_09

--- 백준/10448 유레카 이론.py
+++ 백준/10448 유레카 이론.py
@@ -1,4 +1,20 @@
 num = int(input())
+T = []
 
-for i in range(46):
-    
+for n in range(46):
+    T.append(n*(n+1)//2)
+
+
+def pos(K):
+
+    for i in range(1, 46):
+        for j in range(i, 46):
+            for k in range(j, 46):
+                if T[i]+T[j]+T[k] == K:
+                    return(1)
+    else:
+        return(0)
+
+
+for _ in range(num):
+    print(pos(int(input())))
