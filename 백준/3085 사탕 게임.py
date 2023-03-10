a4295a2c358c1baa3bc8c44b20d03294d74aff61
su <suqkr12@gmail.com>
2023-03-10 23:54:58 +0900

    백준_2023_03_10

---
+++ 백준/3085 사탕 게임.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+num = int(input())
+arr = [list(input()) for _ in range(num)]
+ans = 1
+
+
+def search():
+    global ans
+    for i in range(num):
+        cnt = 1
+        for j in range(1, num):
+            if arr[i][j-1] == arr[i][j]:
+                cnt += 1
+                ans = max(ans, cnt)
+            else:
+                cnt = 1
+        cnt = 1
+    for j in range(num):
+        for i in range(1, num):
+            if arr[i-1][j] == arr[i][j]:
+                cnt += 1
+                ans = max(ans, cnt)
+            else:
+                cnt = 1
+
+
+for i in range(num):
+    for j in range(num):
+        if j+1 < num:
+            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
+            search()
+            arr[i][j], arr[i][j+1] = arr[i][j+1], arr[i][j]
+        if i+1 < num:
+            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
+            search()
+            arr[i][j], arr[i+1][j] = arr[i+1][j], arr[i][j]
+print(ans)
