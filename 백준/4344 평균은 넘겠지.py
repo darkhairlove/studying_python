91364d500bd8a9c02386bbe1b80a55bef6472a9b
su <suqkr12@gmail.com>
2023-06-22 23:44:39 +0900

    백준_2023_06_22

--- 백준/4344 평균은 넘겠지.py
+++ 백준/4344 평균은 넘겠지.py
@@ -1,12 +1,24 @@
-ans = int(input())
-an = 0
-arr = 0
-for i in range(ans):
-    num = list(map(int, input().split()))
-    arr = (sum(num) - num[0]) // num[0]
-    for i in range(1, len(num)):
-        if num[i] > arr:
-            an += 1
-    print("{:.3f}%".format((100*an)/num[0]))
-    arr = 0
-    an = 0
+import sys
+input = sys.stdin.readline
+
+
+def roundTraditional(val, digits):
+    return round(val+10**(-len(str(val))-1), digits)
+
+
+T = int(input())
+
+for _ in range(T):
+    tmp = list(map(int, input().split()))
+    people = tmp[0]
+    scores = tmp[1:]
+
+    avg = sum(scores) / people
+
+    isOver = 0
+    for score in scores:
+        if score > avg:
+            isOver += 1
+
+    result = isOver / people * 100
+    print(f'{roundTraditional(result, 3):.3f}%')
