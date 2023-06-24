4ea9191a4272ff434b18f6b30830e982585e61c4
su <suqkr12@gmail.com>
2023-06-25 01:02:11 +0900

    백준_2023_06_24

---
+++ 백준/1535 안녕.py
@@ -0,0 +1,13 @@
+from sys import stdin
+n = int(stdin.readline())
+stamina = [0] + list(map(int, stdin.readline().split()))
+p = [0] + list(map(int, stdin.readline().split()))
+dp = [[0] * 101 for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, 101):
+        if stamina[i] <= j:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-stamina[i]]+p[i])
+        else:
+            dp[i][j] = dp[i-1][j]
+print(dp[n][99])
