230430af777f6014f7fd4972a2c7d3cbc0d8545c
su <suqkr12@gmail.com>
2023-07-13 22:34:25 +0900

    백준_2023_07_13

---
+++ 백준/11728 배열 합치기.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def read(): return sys.stdin.readline().rstrip()
+
+
+N, M = map(int, read().split())
+
+A = list(map(int, read().split()))
+B = list(map(int, read().split()))
+
+answer = A + B
+answer.sort()
+
+print(*answer)
