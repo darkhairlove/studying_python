16b7076fe44fe696c08391fc6fd74477f8b290ec
su <suqkr12@gmail.com>
2023-07-14 23:44:49 +0900

    백준_2023_07_14

---
+++ 백준/2204 도비의 난독증 테스트.py
@@ -0,0 +1,9 @@
+while True:
+    voca = []
+    n = int(input())
+    if n == 0:
+        quit()
+    for i in range(n):
+        voca.append(input())
+    voca.sort(key=str.lower)
+    print(voca[0])
