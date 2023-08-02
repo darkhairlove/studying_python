5de7a7611f1ea3c463592c1696d039c0bb4b788b
su <suqkr12@gmail.com>
2023-08-03 00:22:29 +0900

    백준_2023_08_02

---
+++ 백준/1544 사이클 단어.py
@@ -0,0 +1,15 @@
+num = int(input())
+arr = []
+for i in range(num):
+    arr.append(input())
+con = 0
+for i in arr:
+    se = []
+    for k in range(len(i)-1):
+        se.append(i[k+1:]+i[0:k+1])
+
+    for m in se:
+        if m in arr and i != m:
+            arr.remove(m)
+
+print(len(set(arr)))
