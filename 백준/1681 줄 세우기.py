7c3b3586ec512500b3e7ef6e9fda73b320b30d11
su <suqkr12@gmail.com>
2023-09-01 00:42:32 +0900

    백준_2023_08_31

---
+++ 백준/1681 줄 세우기.py
@@ -0,0 +1,13 @@
+N, l = list(map(int, input().split(' ')))
+
+num = 1
+cnt = 0
+while True:
+    if str(l) not in str(num):
+        cnt += 1
+        if cnt == N:
+            print(num)
+            break
+        num += 1
+    else:
+        num += 1
